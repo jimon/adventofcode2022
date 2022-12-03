be63d9b45023abcb375c18bd66de2da1519c8749
Dmytro Ivanov <dmytro@unity3d.com>
2022-12-03 12:56:51 +0100

    day1 part2

--- src/day1.rs
+++ src/day1.rs
@@ -5,13 +5,22 @@ use itertools::Itertools;
 pub fn run() {
     let pathname = "input_day1.txt";
     let file = File::open(pathname).expect("can't open file");
-    let max = BufReader::new(file)
+    let mut sums : Vec<i32> = BufReader::new(file)
         .lines()
         .map(|x| x.unwrap_or_default().parse::<i32>().unwrap_or(0))
         .group_by(|x| *x == 0)
         .into_iter()
         .map(|(_, group)| group.sum::<i32>())
-        .max()
+        .collect::<Vec<i32>>();
+
+    sums.sort_by(|a, b| b.cmp(a));
+
+    let part1 = sums
+        .first()
+        .copied()
         .unwrap_or_default();
-    println!("part1 {}", max);
+    println!("part1 {}", part1);
+
+    let part2 = sums[0] + sums[1] + sums[2];
+    println!("part2 {}", part2);
 }
