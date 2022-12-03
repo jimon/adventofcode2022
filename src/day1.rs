7fb9985be30bcb87d66452e18fbf41ed642a8ea5
Dmytro Ivanov <dmytro@unity3d.com>
2022-12-03 11:56:39 +0100

    day1 part1

---
+++ src/day1.rs
@@ -0,0 +1,17 @@
+﻿use std::fs::File;
+use std::io::{BufRead, BufReader};
+use itertools::Itertools;
+
+pub fn run() {
+    let pathname = "input_day1.txt";
+    let file = File::open(pathname).expect("can't open file");
+    let max = BufReader::new(file)
+        .lines()
+        .map(|x| x.unwrap_or_default().parse::<i32>().unwrap_or(0))
+        .group_by(|x| *x == 0)
+        .into_iter()
+        .map(|(_, group)| group.sum::<i32>())
+        .max()
+        .unwrap_or_default();
+    println!("part1 {}", max);
+}
---
+++ src/main.rs
@@ -0,0 +1,7 @@
+mod day1;
+
+fn main() {
+    println!("begin");
+    day1::run();
+    println!("end");
+}
